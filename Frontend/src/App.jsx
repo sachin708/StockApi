cdc83a5228b99da50eaa5e2be4c85dbdbf0027fd
sachinmohite <sachinmohite708@gmail.com>
2024-05-14 09:07:34 +0530

    FrontendDone

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [data, setdata] = useState();
+  let [count, setcount] = useState(0);
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
+  const fetchdata = async () => {
+    try {
+      let res = await fetch("http://localhost:4545/stoke");
+      let data = await res.json();
+      setdata(data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // setTimeout(() => {
+  //   setcount(() => count++);
+  // }, 1000);
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Stock Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Buy</th>
+            <th>Sell</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((ele, index) => (
+            <tr key={index}>
+              <td>{ele.name}</td>
+
+              <td>
+                {count < 10
+                  ? (ele.price,
+                    setTimeout(() => {
+                      setcount(() => count++);
+                    }, 4000))
+                  : (ele.price,
+                    setTimeout(() => {
+                      setcount(() => count--);
+                    }, 2000))}
+              </td>
+              <td>{ele.quantity}</td>
+              <td
+                style={{
+                  backgroundColor: "green",
+                  color: "white",
+                  borderRadius: "4px",
+                  padding: "8px",
+                }}
+              >
+                Buy
+              </td>
+              <td
+                style={{
+                  backgroundColor: "blue",
+                  color: "white",
+                  borderRadius: "4px",
+                  padding: "8px",
+                }}
+              >
+                Sell
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default App;
